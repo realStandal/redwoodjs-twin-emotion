0446278fe4e10aab8c5ccf26e34c6cf636e953ff
Ryan <jesselockard99@gmail.com>
2021-03-24 04:50:34 -0400

    added Babel configuration for Twin.Macro

--- web/.babelrc.js
+++ web/.babelrc.js
@@ -1,1 +1,21 @@
-module.exports = { extends: "../babel.config.js" }
+module.exports = {
+  extends: "../babel.config.js",
+  plugins: [
+    "babel-plugin-macros",
+    [
+      "@emotion/babel-plugin-jsx-pragmatic",
+      {
+        export: "jsx",
+        import: "__cssprop",
+        module: "@emotion/react",
+      }
+    ],
+    [
+      "@babel/plugin-transform-react-jsx",
+      {
+        pragma: "__cssprop",
+        pragmaFrag: "React.Fragment"
+      }
+    ]
+  ]
+}
